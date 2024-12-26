c52b34db1a059536b82b10a129e1ac5480579f6b
Роман Кораблев <korabl79@mail.ru>
2024-12-26 07:07:19 +0300

    removed the created form and Django created it automatically

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
 from .forms import BirthdayForm
+from .utils import calculate_birthday_countdown
 
 
 def birthday(request):
-    form = BirthdayForm()
+    form = BirthdayForm(request.GET or None)
+    birthday_countdown = None
     context = {'form': form}
+    if form.is_valid():
+        # ...вызовем функцию подсчёта дней:
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+
+    context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context=context)
